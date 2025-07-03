a413d386fe430cd77313d3dbc3677b814e086ec1
Vorn-Zz <Sothunvorn@gmail.com>
2025-07-03 11:26:31 +0700

    update

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, GitHub Actions!');
+  res.send('Nihao, GitHub Actions!');
 });
 
 app.listen(port, () => {
